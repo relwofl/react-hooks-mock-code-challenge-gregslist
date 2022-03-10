a138fe35e8e7498a4dad42319dbbcc0ca9c9ee18
Luke Fowler <luciusfow@gmail.com>
2022-03-09 23:47:05 -0500

    commit tree 0a2ab8eef4dc0e0586a5e9311b0c10f38731b1da

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,35 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+// import { useEffect } from "react/cjs/react.production.min";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ search }) {
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then((r) =>r.json())
+    .then((listings) => setListings(listings))
+  }, [] )
+  
+  function handleDelete(id) {
+    const updatedlistArray = listings.filter(listing => listing.id !== id)
+    setListings(updatedlistArray)
+
+  }
+
+  const filterArray = listings.filter(listing => {
+    return listing.description.toLowerCase().includes(search.toLowerCase())
+  })
+
+console.log(filterArray)
+
+  const listingCards = filterArray.map(listingObj => {
+    return <ListingCard key={listingObj.id} listing={listingObj} onDelete={handleDelete}/>
+  })
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
